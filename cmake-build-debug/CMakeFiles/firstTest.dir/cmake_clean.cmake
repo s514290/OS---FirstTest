c508c539c457e07c198d4dd5ca3c1ccdf9abbb92
Chester <s514290@nwmissouri.edu>
2017-09-15 13:31:53 -0500

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/firstTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstTest.dir/main.cpp.obj"
+  "firstTest.pdb"
+  "firstTest.exe"
+  "firstTest.exe.manifest"
+  "libfirstTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
